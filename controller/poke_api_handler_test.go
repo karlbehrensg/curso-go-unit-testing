1bcd94b9c42eb463e46ef2dc16444c506c8d20ed
Karl Behrens <karlbehrensg@gmail.com>
2022-10-19 22:41:13 -0300

    Utilizando mocks

---
+++ controller/poke_api_handler_test.go
@@ -0,0 +1,111 @@
+package controller
+
+import (
+	"catching-pokemons/models"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetPokemonFromPokeApiSuccess(t *testing.T) {
+	c := require.New(t)
+
+	pokemon, err := GetPokemonFromPokeApi("bulbasaur")
+	c.NoError(err)
+
+	body, err := ioutil.ReadFile("samples/poke_api_read.json")
+	c.NoError(err)
+
+	var expected models.PokeApiPokemonResponse
+
+	err = json.Unmarshal([]byte(body), &expected)
+	c.NoError(err)
+
+	c.Equal(expected, pokemon)
+}
+
+func TestGetPokemonFromPokeApiSuccessWithMock(t *testing.T) {
+	c := require.New(t)
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	id := "bulbasaur"
+
+	body, err := ioutil.ReadFile("samples/poke_api_response.json")
+	c.NoError(err)
+
+	request := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s", id)
+
+	httpmock.RegisterResponder("GET", request, httpmock.NewStringResponder(200, string(body)))
+
+	pokemon, err := GetPokemonFromPokeApi(id)
+	c.NoError(err)
+
+	var expected models.PokeApiPokemonResponse
+
+	err = json.Unmarshal([]byte(body), &expected)
+	c.NoError(err)
+
+	c.Equal(expected, pokemon)
+}
+
+func TestGetPokemonFromPokeApiInternalServerError(t *testing.T) {
+	c := require.New(t)
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	id := "bulbasaur"
+
+	body, err := ioutil.ReadFile("samples/poke_api_response.json")
+	c.NoError(err)
+
+	request := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s", id)
+
+	httpmock.RegisterResponder("GET", request, httpmock.NewStringResponder(500, string(body)))
+
+	_, err = GetPokemonFromPokeApi(id)
+	c.NotNil(err)
+	c.EqualError(ErrPokeApiFailure, err.Error())
+}
+
+func TestGetPokemon(t *testing.T) {
+	c := require.New(t)
+
+	r, err := http.NewRequest("GET", "/pokemon/{id}", nil)
+	c.NoError(err)
+
+	w := httptest.NewRecorder()
+
+	vars := map[string]string{
+		"id": "bulbasaur",
+	}
+
+	r = mux.SetURLVars(r, vars)
+
+	GetPokemon(w, r)
+
+	expectedBodyResponse, err := ioutil.ReadFile("samples/api_response.json")
+	c.NoError(err)
+
+	var expectedPokemon models.Pokemon
+
+	err = json.Unmarshal([]byte(expectedBodyResponse), &expectedPokemon)
+	c.NoError(err)
+
+	var actualPokemon models.Pokemon
+
+	err = json.Unmarshal([]byte(w.Body.Bytes()), &actualPokemon)
+	c.NoError(err)
+
+	c.Equal(http.StatusOK, w.Code)
+	c.Equal(expectedPokemon, actualPokemon)
+}
